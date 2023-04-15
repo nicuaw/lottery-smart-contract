7c20ecfb0cd30fae0cbd7084e106d44137307b05
wasen2 <wasen2@bfh.ch>
2023-04-15 13:39:02 +0200

    modified seedgen and testing

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,35 +1,22 @@
+// based on template of https://ethereum.org/en/developers/tutorials/hello-world-smart-contract/
+
 require('dotenv').config();
-
-
 require("@nomiclabs/hardhat-ethers");
 
 const { API_URL, PRIVATE_KEY } = process.env;
 
-
 /**
-
 * @type import('hardhat/config').HardhatUserConfig
-
 */
 
 module.exports = {
-
   solidity: "0.8.18",
-
   defaultNetwork: "goerli",
-
   networks: {
-
     hardhat: {},
-
     goerli: {
-
       url: `${API_URL}`,
-
       account: PRIVATE_KEY
-
     }
-
   },
-
 }
